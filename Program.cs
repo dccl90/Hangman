4bb1e500bd7230af80083b76b47b91393aefe320
Dean Clarke <dclarke@twilio.com>
2024-03-04 22:54:57 +1100

    Add validation to keep track of guesses and confirm the correct input

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        //const int GUESSES = 6;
+        const int MAX_GUESSES = 5;
+        const int EMPTY_LIST_CHECK = 0;
+
         List<string> words = new List<string>()
         {
             "bed",
@@ -13,34 +15,69 @@ class Program
             "zebra",
             "hippopotamus"
         };
-
-        List<char> characters = new List<char>(); 
-
+        List<char> enteredLetters = new List<char>(); 
+        List<char> correctLetters = new List<char>();   
         string selectedWord = words[new Random().Next(0, words.Count -1)];
-        
+        Console.Clear();
         Console.WriteLine("#################");
         Console.WriteLine("#####HANGMAN#####");
         Console.WriteLine("#################");
-        Console.WriteLine("-----------------");
-        while(true) 
-        {
 
-            Console.Write("Enter a Letter: ");
-            string input = Console.ReadLine();
-            characters.Add(input.ToCharArray()[0]);
+        int guesses = 0;
+        while(true) 
+        {   
 
             foreach(char c in selectedWord) 
             {
-                if(characters.Contains(c))
+                if(enteredLetters.Contains(c))
                 {
                     Console.Write(c);
                 } 
                 else 
                 {
-                    Console.Write('_');   
+                    Console.Write('_');      
                 }
+
+                if(enteredLetters.Count > EMPTY_LIST_CHECK && enteredLetters.Last() == c){
+                    correctLetters.Add(c);
+                }
+                
             }
-            Console.WriteLine("");
+            Console.WriteLine(string.Empty);
+            
+            if(guesses == MAX_GUESSES)
+            {
+              Console.WriteLine("#################");
+              Console.WriteLine("###GAME OVER!####");
+              Console.WriteLine($"The word was {selectedWord}");
+              Console.WriteLine("#################");
+              break;
+            } 
+
+            if(correctLetters.Count == selectedWord.Length)
+            {   
+                Console.WriteLine("#################");
+                Console.WriteLine($"You won with { MAX_GUESSES - guesses} guesses remaining");
+                Console.WriteLine("#################");
+                break;
+            }
+
+            Console.Write("Enter a Letter: ");
+            char input = Console.ReadKey().KeyChar;
+            Console.WriteLine(string.Empty);
+
+            if(enteredLetters.Contains(input))
+            {
+                Console.WriteLine($"You have already entered letter {input}");
+                continue;
+            }
+
+            enteredLetters.Add(input);
+
+            if(!selectedWord.Contains(input))
+            {
+              guesses++;  
+            } 
         }
     }
 }
